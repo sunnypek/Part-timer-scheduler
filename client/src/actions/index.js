8f099403024c7750e660549244dd34b63d7fc37d
Sunny Pek <sunny1996@live.com.sg>
2021-02-01 02:21:52 +0800

    replace checkbox with actual value before post

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -11,19 +11,23 @@ export const signUp = (data) => {
 	*/
 	return async (dispatch) => {
 		try {
-			const res = await axios.post("http://localhost:1337/users/signup", data);
+				if (data.authLevel) {
+					data.authLevel = "admin";
+				} else {
+					data.authLevel = "user";
+				}
+				const res = await axios.post("http://localhost:1337/users/signup", data);
+				dispatch({
+					type: AUTH_SIGN_UP,
+					payload: res.data.token
+				});
 
-			dispatch({
-				type: AUTH_SIGN_UP,
-				payload: res.data.token
-			});
-
-			localStorage.setItem("JWT_TOKEN", res.data.token);
+				localStorage.setItem("JWT_TOKEN", res.data.token);
 		} catch (error) {
 			dispatch({
 				type: AUTH_ERROR,
 				payload: "Email is already in use"
-			});
+			})
 		}
 	}
-}
+};
