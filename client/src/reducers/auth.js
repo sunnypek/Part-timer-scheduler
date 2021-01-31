3583f1127c5dec6fd6f329e0b5e604376f813484
Sunny Pek <sunny1996@live.com.sg>
2021-02-01 02:21:15 +0800

    added switch case for auth reducer

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,11 +1,22 @@
+import { AUTH_SIGN_UP, AUTH_ERROR } from "../actions/types";
+
 const DEFAULT_STATE = {
 	isAuthenticated: false,
 	token: "",
-	errorMessage: "",
+	errorMessage: ""
 }
 
 const authReducer = (state = DEFAULT_STATE, action) => {
-	return state;
-};
+	switch(action.type) {
+		case AUTH_SIGN_UP:
+			console.log(`auth sign up action`);
+			return { ...state, token: action.payload, isAuthenticated: true, errorMessage: "" };
+		case AUTH_ERROR:
+			console.log(`auth error`);
+			return { ...state, errorMessage: action.payload };
+		default:
+			return state;
+	}
+}
 
 export default authReducer;
