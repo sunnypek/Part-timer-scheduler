ed780bce28b827ecb3cac612e5baa619e497f536
Sunny Pek <sunny1996@live.com.sg>
2021-01-31 22:40:38 +0800

    added signup actioncreator

---
+++ client/src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+export const signUp = (data) => {
+	/** 
+	 * 1. use data to make HTTP request to backend 
+	 * 2. take backend response (JWT)
+	 * 3. dispatch user signed up with JWT
+	 * 4. save JWT to local storage 
+	*/
+	return async (dispatch) => {
+		try {
+			
+		} catch (error) {
+			dispatch({});
+		}
+	}
+}
