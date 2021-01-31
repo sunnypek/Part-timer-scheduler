66adfe69998545a0a16aaf8ba52fbf435f07bab0
Sunny Pek <sunny1996@live.com.sg>
2021-01-31 22:43:49 +0800

    axios post request to server for signup

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+import { AUTH_ERROR, AUTH_SIGN_UP } from "./types";
+
 export const signUp = (data) => {
 	/** 
 	 * 1. use data to make HTTP request to backend 
@@ -9,9 +11,19 @@ export const signUp = (data) => {
 	*/
 	return async (dispatch) => {
 		try {
-			
+			const res = await axios.post("http://localhost:1337/users/signup", data);
+
+			dispatch({
+				type: AUTH_SIGN_UP,
+				payload: res.data.token
+			});
+
+			localStorage.setItem("JWT_TOKEN", res.data.token);
 		} catch (error) {
-			dispatch({});
+			dispatch({
+				type: AUTH_ERROR,
+				payload: "Email is already in use"
+			});
 		}
 	}
 }
