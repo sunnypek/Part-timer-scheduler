8ee50773c00d5665d7ddf0617cc7f8fae487beb0
Sunny Pek <sunny1996@live.com.sg>
2021-02-02 17:30:15 +0800

    changed errorMessage to message (handle different states)

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -3,15 +3,15 @@ import { AUTH_SIGN_UP, AUTH_ERROR } from "../actions/types";
 const DEFAULT_STATE = {
 	isAuthenticated: false,
 	token: "",
-	errorMessage: ""
+	message: ""
 }
 
 const authReducer = (state = DEFAULT_STATE, action) => {
 	switch(action.type) {
 		case AUTH_SIGN_UP:
-			return { ...state, token: action.payload, isAuthenticated: true, errorMessage: "" };
+			return { ...state, token: action.payload, isAuthenticated: true, message: "SIGN_UP" };
 		case AUTH_ERROR:
-			return { ...state, errorMessage: action.payload };
+			return { ...state, message: action.payload };
 		default:
 			return state;
 	}
