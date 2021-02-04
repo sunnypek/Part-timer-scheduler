6dd0789ce2f4022da5db07d0bac93706a79e3466
Sunny Pek <sunny1996@live.com.sg>
2021-02-04 21:37:10 +0800

    added working login page

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,11 +1,98 @@
-import React from "react";
+import React, { Component } from "react";
+import { reduxForm, Field } from "redux-form";
+import { connect } from "react-redux";
+import { compose } from "redux";
 
-const Login = () => {
-	return(
-		<div>
-			Login page
-		</div>
-	)
+import * as actions from "../actions";
+import CustomInput from "./CustomInput";
+
+class Login extends Component {
+	constructor(props) {
+		super(props);
+		this.onLoginSubmit = this.onLoginSubmit.bind(this);
+	}
+
+	async onLoginSubmit(formData) {
+		await this.props.login(formData);
+		if (!this.props.err) {
+			this.props.history.push("/");
+		}
+	}
+	
+	render() {
+		const { handleSubmit } = this.props;
+
+		let alert;
+		if (this.props.err) {
+			switch (this.props.message) {
+				case "SIGN_UP_NO_EMAIL":
+					alert = <div className="alert alert-danger">Email is missing!</div>;
+					break;
+				case "SIGN_UP_PASSWORD_LENGTH_ERROR":
+					alert = <div className="alert alert-danger">Password must be min 6 and max 18!</div>;
+					break;
+				case "SIGN_UP_NO_PASSWORD_ERROR":
+					alert = <div className="alert alert-danger">Password is missing!</div>;
+					break;
+				case "SIGN_UP_NO_DETAILS":
+					alert = <div className="alert alert-info">Please enter your details.</div>;
+					break;
+				default:
+					alert = <div className="alert alert-danger">No account found / Password is wrong!</div>;
+					break;
+			}
+		} else {
+			if (this.props.message === "SIGN_UP") {
+				alert = <div className="alert alert-success">Successfully signed up!</div>;
+			} else {
+				alert = "";
+			}
+		}
+
+		return(
+			<div className="row justify-content-center">
+				<div className="col-4">
+					<div className="alert alert-success text-center">
+						Login
+					</div>
+					<form onSubmit={ handleSubmit(this.onLoginSubmit) }>
+						<fieldset>
+							<Field
+								name="email"
+								type="text"
+								id="email"
+								label="Email"
+								placeholder="example@example.com"
+								component= { CustomInput } />
+						</fieldset>
+						<fieldset>
+							<Field
+								name="password"
+								type="password"
+								id="password"
+								label="Password"
+								placeholder=""
+								component= { CustomInput } />
+						</fieldset>
+
+						{ alert }
+
+						<button type="submit" className="btn btn-primary">Login</button>
+					</form>
+				</div>
+			</div>
+		)
+	}
 };
 
-export default Login;
+function MapStateToProps(state) {
+	return {
+		message: state.auth.message,
+		err: state.auth.err
+	}
+}
+
+export default compose(
+	connect(MapStateToProps, actions),
+	reduxForm({ form: "login" })
+)(Login);
