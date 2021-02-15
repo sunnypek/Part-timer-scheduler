83a647a9c522e42fa6f61ed188e73778b6f1e045
Sunny Pek <sunny1996@live.com.sg>
2021-02-15 12:17:09 +0800

    change authReducer to handle authLevel

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -4,15 +4,16 @@ const DEFAULT_STATE = {
 	isAuthenticated: false,
 	token: "",
 	message: "",
-	err: false
+	err: false,
+	authLevel: "user"
 }
 
 const authReducer = (state = DEFAULT_STATE, action) => {
 	switch(action.type) {
 		case AUTH_LOGIN:
-			return { ...state, token: action.payload, isAuthenticated: true, message: "LOGIN", err: false};
+			return { ...state, token: action.payload, isAuthenticated: true, message: "LOGIN", err: false, authLevel: action.authLevel};
 		case AUTH_SIGN_UP:
-			return { ...state, token: action.payload, isAuthenticated: true, message: "SIGN_UP", err: false };
+			return { ...state, token: action.payload, isAuthenticated: true, message: "SIGN_UP", err: false, authLevel: action.authLevel };
 		case AUTH_LOGOUT:
 			return { ...state, token: action.payload, isAuthenticated: false, message: "LOGOUT", err: false};
 		case AUTH_ERROR:
