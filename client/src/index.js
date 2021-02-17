6fe52ffa0552b637a27729afc20df794b1a06c6e
Sunny Pek <sunny1996@live.com.sg>
2021-02-17 22:13:11 +0800

    authGuard for home page

--- client/src/index.js
+++ client/src/index.js
@@ -12,6 +12,7 @@ import Signup from "./Components/Signup";
 import Login from "./Components/Login";
 import Logout from "./Components/Logout";
 import reducers from "./reducers";
+import authGuard from "./Components/HOCs/authGuard";
 
 const jwtToken = localStorage.getItem("JWT_TOKEN");
 const authLevel = localStorage.getItem("AUTH_LEVEL");
@@ -26,7 +27,7 @@ ReactDOM.render(
 	}, applyMiddleware(reduxThunk)) }>
 		<BrowserRouter>
 			<App>
-				<Route exact path="/home" component={Home} />
+				<Route exact path="/home" component={authGuard(Home)} />
 				<Route exact path="/" component={Signup} />
 				<Route exact path="/login" component={Login} />
 				<Route exact path="/logout" component={Logout} />
