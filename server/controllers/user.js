e935d95be9bbc2de0fcf5f8f3f1285e6679ba055
Sunny Pek <sunny1996@live.com.sg>
2021-02-19 16:38:50 +0800

    added username for signup/login server calls

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -33,7 +33,8 @@ module.exports = {
 
 		res.status(200).json({ 
 			token: token,
-			authLevel: newUser.authLevel
+			authLevel: newUser.authLevel,
+			username: newUser.username
 		});
 	},
 
@@ -41,7 +42,8 @@ module.exports = {
 		const token = signToken(req.user);
 		res.status(200).json({ 
 			token: token,
-			authLevel: req.user.authLevel 
+			authLevel: req.user.authLevel,
+			username: req.user.username
 		});
 	}
 }
