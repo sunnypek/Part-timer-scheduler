c60e19b45f54d3aaea708a9046f7b002f5caa4bd
Sunny Pek <sunny1996@live.com.sg>
2021-03-02 12:12:33 +0800

    changed clockin function to insert into correct table and return original details

--- server/controllers/database.js
+++ server/controllers/database.js
@@ -4,8 +4,8 @@ module.exports = {
 	clockIn: async (req, res, next) => {
 		console.log("req.body is ", req.body);
 		const result = await db.execute(
-			"INSERT INTO timeslot (TimeSlot_ID, Start_DateTime, End_DateTime, Normal_Rate, OT_Rate) VALUES (?, ?, ?, ?, ?)"
-			,[req.body.timeSlotID, req.body.startDateTime, req.body.endDateTime, req.body.normalRate, req.body.OTRate]
+			"INSERT INTO bookingdetail (Timeslot_ID, Employee_Name, Clock_IN, Clock_OUT, Normal_hr, OverTime_hr) VALUES (?, ?, ?, ?, ?, ?)"
+			,[req.body.timeslotID, req.body.employeeName, req.body.clockIn, req.body.clockOut, req.body.normalHour, req.body.overtimeHour]
 		);
 		console.log("result is ", result);
 		res.status(200).json({ 
