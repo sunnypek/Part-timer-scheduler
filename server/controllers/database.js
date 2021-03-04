f95cd949840913303eee5fb4e568a6097d97b4bc
Sunny Pek <sunny1996@live.com.sg>
2021-03-04 14:05:41 +0800

    updated database API to return err when present

--- server/controllers/database.js
+++ server/controllers/database.js
@@ -3,36 +3,48 @@ const db = require("../database");
 module.exports = {
 	clockIn: (req, res, next) => {
 		db.execute(
-			"INSERT INTO bookingdetail (Timeslot_ID, Employee_Name, Clock_IN, Clock_OUT, Normal_hr, OverTime_hr) VALUES (?, ?, ?, ?, ?, ?)"
-			,[req.body.timeslotID, req.body.employeeName, req.body.clockIn, null, req.body.normalHour, req.body.overtimeHour]
+			"INSERT INTO bookingdetail (Timeslot_ID, Employee_Name, Clock_IN, Clock_OUT, Normal_hr, OverTime_hr) VALUES (?, ?, ?, ?, ?, ?)",
+			[req.body.timeslotID, req.body.employeeName, req.body.clockIn, null, req.body.normalHour, req.body.overtimeHour],
+			(err, results, fields) => {
+				if (err) {
+					res.status(400).json({ err });
+				} else {
+					res.status(200).json({ 
+						timeslotID: req.body.timeslotID,
+						employeeName: req.body.employeeName,
+						clockIn: req.body.clockIn,
+						clockOut: null,
+						normalHour: req.body.normalHour,
+						overtimeHour: req.body.overtimeHour
+					});
+				}
+			}
 		);
-		res.status(200).json({ 
-			timeslotID: req.body.timeslotID,
-			employeeName: req.body.employeeName,
-			clockIn: req.body.clockIn,
-			clockOut: null,
-			normalHour: req.body.normalHour,
-			overtimeHour: req.body.overtimeHour
-		});
 	},
 
 	clockOut: (req, res, next) => {
 		db.execute(
 			"UPDATE bookingdetail SET Clock_Out = ? WHERE Timeslot_ID = ? AND Employee_Name = ?",
-			[req.body.clockOut, req.body.timeslotID, XMLHttpRequestEventTarget.body.employeeName]
+			[req.body.clockOut, req.body.timeslotID, XMLHttpRequestEventTarget.body.employeeName],
+			(err, results, fields) => {
+				if (err) {
+					res.status(400).json({ err });
+				} else {
+					res.status(200).json({
+						clockOut: req.body.clockOut,
+						timeslotID: req.body.timeslotID,
+						employeeName: req.body.employeeName		
+					});
+				}
+			}
 		);
-		res.status(200).json({
-			clockOut: req.body.clockOut,
-			timeslotID: req.body.timeslotID,
-			employeeName: req.body.employeeName		
-		});
 	},
 
 	getEmployees: (req, res, next) => {
 		db.execute(
 			"SELECT * FROM userinfo", 
 			(err, results, fields) => {
-				err ? console.error(err) : res.status(200).json({ results });
+				err ? res.status(400).json({ err }) : res.status(200).json({ results });
 			}
 		);
 	},
@@ -41,7 +53,7 @@ module.exports = {
 		db.execute(
 			"SELECT Timeslot_ID, Clock_IN, Clock_OUT, Normal_hr, OverTime_hr FROM bookingdetail", 
 			(err, results, fields) => {
-				err ? console.error(err) : res.status(200).json({ results });
+				err ? res.status(400).json({ err }) : res.status(200).json({ results });
 			}
 		);
 	},
