3c31b38bfd15aced6007b6a12c64eb0d5bc2d594
Sunny Pek <sunny1996@live.com.sg>
2021-03-12 03:21:11 +0800

    alter custom timeslot input to be dynamic for required

--- client/src/Components/addTimeslotInput.js
+++ client/src/Components/addTimeslotInput.js
@@ -3,22 +3,34 @@ import React, { Component } from "react";
 export default class AddTimeslotInput extends Component {
 	render() {
 		const { input: { value, onChange }} = this.props;
+		let field = "";
+		if (this.props.alt === "req") {
+			field = <input 
+			name = { this.props.name }
+			id = { this.props.id }
+			placeholder = { this.props.placeholder }
+			className = "form-control"
+			type = { this.props.type }
+			value = { value }
+			onChange = { onChange }
+			required />
+		} else {
+			field = <input 
+			name = { this.props.name }
+			id = { this.props.id }
+			placeholder = { this.props.placeholder }
+			className = "form-control"
+			type = { this.props.type }
+			value = { value }
+			onChange = { onChange } />
+		}
 		return(
 				<React.Fragment>
 					<div className="col-3">
 						<label className="col-form-label font-weight-bold" htmlFor={this.props.id}>{this.props.label}:</label>
 					</div>
 					<div className="col-9">
-						<input 
-							name = { this.props.name }
-							id = { this.props.id }
-							placeholder = { this.props.placeholder }
-							className = "form-control"
-							type = { this.props.type }
-							value = { value }
-							onChange = { onChange }
-							required
-						/>
+						{field}
 					</div>
 				</React.Fragment>
 		);
