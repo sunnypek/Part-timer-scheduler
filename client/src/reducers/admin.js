d7b1b020df5dfc0bc7f06d744e725fc889ee0999
Sunny Pek <sunny1996@live.com.sg>
2021-03-11 20:34:55 +0800

    added new case to reducer

--- client/src/reducers/admin.js
+++ client/src/reducers/admin.js
@@ -1,4 +1,4 @@
-import { ADD_TIMESLOT, ADD_TIMESLOT_ERROR } from "../actions/types";
+import { ADD_TIMESLOT, ADD_TIMESLOT_ERROR, UI_CHANGE } from "../actions/types";
 
 const DEFAULT_STATE = {
 	message: "",
@@ -8,9 +8,11 @@ const DEFAULT_STATE = {
 const adminReducer = (state = DEFAULT_STATE, action) => {
 	switch(action.type) {
 		case ADD_TIMESLOT:
-			return { message: action.payload, err: false};
+			return { message: action.payload, err: false };
 		case ADD_TIMESLOT_ERROR:
 			return { message: action.payload, err: true };
+		case UI_CHANGE:
+			return { message: action.payload, err: false };
 		default:
 			return state;
 	}
