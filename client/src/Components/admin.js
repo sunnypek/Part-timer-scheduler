86d775f6b6a86cde7f74ae29d9b8acc63fa1a80e
Sunny Pek <sunny1996@live.com.sg>
2021-03-19 00:16:59 +0800

    edit and delete directly from popup

--- client/src/Components/admin.js
+++ client/src/Components/admin.js
@@ -5,6 +5,7 @@ import moment from "moment";
 import { connect } from "react-redux";
 import { compose } from "redux";
 import { reduxForm, Field } from "redux-form";
+import Swal from "sweetalert2";
 // eslint-disable-next-line
 import styles from 'react-big-calendar/lib/css/react-big-calendar.css';
 
@@ -50,6 +51,75 @@ class Admin extends Component {
 		this.setState({ gotData: true });
 	}
 
+	async selectEvent(event) {
+		Swal.fire({
+            title: event.title,
+            html: `
+                Time: <span style="color: #e67e22">${event.start}</span> to <span style="color: #e74c3c">${event.end}</span><br/>
+                Normal Rate: <strong style="color: #16a085">${event.normalRate}</strong><br/>
+                Overtime Rate: <strong style="color: #f39c12">${event.overtimeRate}</strong>`,
+            showDenyButton: true,
+			showCancelButton: true,
+            confirmButtonText: `Edit`,
+            denyButtonText: `Delete`,
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                Swal.fire({
+					title: "Edit timeslot",
+					html: `
+						<div class="row">
+							<div class="col-6">
+								<label class="col-form-label font-weight-bold">Start D/T</label>
+								<input id="editEventStart"  value="${event.start}" class = "form-control" />
+							</div>
+							<div class="col-6">
+								<label class="col-form-label font-weight-bold">End D/T</label>
+								<input id="editEventEnd"  value="${event.end}" class = "form-control" />
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-6">
+								<label class="col-form-label font-weight-bold">Normal Rate</label>
+								<input id="editEventNormalRate"  value="${event.normalRate}" class = "form-control" />
+							</div>
+							<div class="col-6">
+								<label class="col-form-label font-weight-bold">OT Rate</label>
+								<input id="editEventOTRate"  value="${event.overtimeRate}" class = "form-control" />
+							</div>
+						</div>`,
+						showCancelButton: true,
+						confirmButtonText: `Confirm Edit`
+				}).then(async (result) => {
+					if (result.isConfirmed) {
+						const updateData = {
+							TimeSlot_ID: event.title,
+							Start_DateTime: document.getElementById("editEventStart").value,
+							End_DateTime: document.getElementById("editEventEnd").value,
+							Normal_Rate: document.getElementById("editEventNormalRate").value,
+							OT_Rate: document.getElementById("editEventOTRate").value
+						}
+						await axios.patch("http://localhost:1337/database/timeslot", updateData);
+						window.location.reload();
+					}
+				});
+            } 
+			if (result.isDenied) {
+				Swal.fire({
+					icon: "warning",
+					text: "Are you sure?",
+					confirmButtonText: "No",
+					showDenyButton: true,
+					denyButtonText: "Yes"
+				}).then(async (result) => {
+					if (result.isDenied) {
+						await axios.delete(`http://localhost:1337/database/timeslot?timeslotID=${event.title}`);
+						window.location.reload();
+					}
+				});
+			}
+        });
+	}
+
 	render() {
 		const { handleSubmit } = this.props;
 
@@ -58,12 +128,6 @@ class Admin extends Component {
 			switch (this.props.message) {
 				case "ADD_TIMESLOT_ERROR":
 					alert = <div className="alert alert-danger text-center d-flex justify-content-center" role="alert">Please check that all data are entered correctly! (make sure ID is not in use)</div>;
-					break;
-				case "PATCH_TIMESLOT_ERROR":
-					alert = <div className="alert alert-danger text-center d-flex justify-content-center" role="alert">Timeslot ID and 1 other fields are required!</div>;
-					break;
-				case "DELETE_TIMESLOT_ERROR":
-					alert = <div className="alert alert-danger text-center d-flex justify-content-center" role="alert">Check that the timeslot ID exist!</div>;
 					break;
 				default:
 					break;
@@ -104,53 +168,9 @@ class Admin extends Component {
 							<button type = "button" className="searchBtn" onClick={this.props.cancelButton}>Cancel</button>
 					</div>
 				</form>
-		} else if (this.props.message === "CLICK_TIME_SLOT_EDIT" || this.props.message === "PATCH_TIMESLOT_ERROR") {
-			addReleaseUI = 
-				<form id="editTimeslotForm" className="needs-validation" onSubmit= { handleSubmit(this.onTimeslotEdit) }>
-					<div className="row">
-						<div className="col-6 row">
-							<Field alt="req" name="TimeSlot_ID" type="text" id="timeslotID_edit" label="Timeslot ID" placeholder="Enter a timeslot ID" component={ AddTimeslotInput } />
-						</div>
-					</div>
-					<div className="row">
-						<div className="col-6 row">
-							<Field name="Start_DateTime" type="text" id="startDateTime_edit" label="Start D/T" placeholder="Format (24H) - YYYY-MM-DD HH:MM:SS" component={ AddTimeslotInput } />
-						</div>
-						<div className="col-6 row">
-							<Field name="End_DateTime" type="text" id="endDateTime_edit" label="End D/T" placeholder="Format (24H) - YYYY-MM-DD HH:MM:SS" component={ AddTimeslotInput } />
-						</div>
-					</div>
-					<div className="row">
-						<div className="col-6 row">
-							<Field name="Normal_Rate" type="number" id="normalRate_edit" label="Normal rate" placeholder="Normal rate per hour for this job" component={ AddTimeslotInput } />
-						</div>
-						<div className="col-6 row">
-							<Field name="OT_Rate" type="number" id="overtimeRate_edit" label="OT rate" placeholder="Overtime rate per hour for this job" component={ AddTimeslotInput } />
-						</div>
-					</div>
-					<div className="row"><div className="col-3"></div><div className="col-6">{alert}</div><div className="col-3"></div></div>
-					<div className = "row">
-							<button type = "submit" className="searchBtn">Edit Timeslot</button>
-							<button type = "button" className="searchBtn" onClick={this.props.cancelButton}>Cancel</button>
-					</div>
-				</form>
-		} else if (this.props.message === "CLICK_TIME_SLOT_DELETE" || this.props.message === "DELETE_TIMESLOT_ERROR") {
-			addReleaseUI = 
-				<form id="deleteTimeslotForm" className="needs-validation" onSubmit= { handleSubmit(this.onTimeslotDelete) }>
-					<div className="row">
-						<div className="col-6 row">
-							<Field alt="req" name="TimeSlot_ID" type="text" id="timeslotID_delete" label="Timeslot ID" placeholder="Enter a timeslot ID" component={ AddTimeslotInput } />
-						</div>
-					</div>
-					<div className="row"><div className="col-3"></div><div className="col-6">{alert}</div><div className="col-3"></div></div>
-					<div className = "row">
-							<button type = "submit" className="searchBtn">Delete Timeslot</button>
-							<button type = "button" className="searchBtn" onClick={this.props.cancelButton}>Cancel</button>
-					</div>
-				</form>
 		} else if (this.props.message === "CANCEL" || this.props.message === "" || this.props.message === "SUCCESS") {
 			if (this.state.gotData) {
-				addReleaseUI = <Calendar popup localizer={local} events={eventsList} startAccessor="start" endAccessor="end" style={{minHeight: 500}} drilldownView="agenda" views={['month','agenda']} />
+				addReleaseUI = <Calendar popup  localizer={local} onSelectEvent={this.selectEvent} events={eventsList} startAccessor="start" endAccessor="end" style={{minHeight: 500}} views={['month']} />
 			} else {
 				addReleaseUI = <div className="d-flex justify-content-center">
 				<div className="spinner-border" role="status">
@@ -197,8 +217,6 @@ class Admin extends Component {
 									</div>
 									<div className="d-flex flex-column col-2 justify-content-center">
 										<button type="button" onClick={this.props.clickAddTimeslot} className="btn btn-primary btn-lg my-2">Add Timeslot</button>
-										<button type="button" onClick={this.props.clickEditTimeslot} className="btn btn-success btn-lg my-2">Edit Timeslot</button>
-										<button type="button" onClick={this.props.clickDeleteTimeslot} className="btn btn-danger btn-lg my-2">Delete Timeslot</button>
 									</div>
 								</div>
 							</div>
