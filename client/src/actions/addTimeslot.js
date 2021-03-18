11d28403b57af6f44d3effb5d2b8d3b0ae0a6781
Sunny Pek <sunny1996@live.com.sg>
2021-03-19 00:18:55 +0800

    removed unused actions

--- client/src/actions/addTimeslot.js
+++ client/src/actions/addTimeslot.js
@@ -19,50 +19,8 @@ export const addTimeslot = (data) => {
 	}
 };
 
-export const editTimeslot = (data) => {
-	return async (dispatch) => {
-		try {
-			await axios.patch("http://localhost:1337/database/timeslot", data);
-			dispatch({
-				type: ADD_TIMESLOT,
-				payload: "SUCCESS"
-			});
-		} catch (error) {
-			dispatch({
-				type: ADD_TIMESLOT_ERROR,
-				payload: "PATCH_TIMESLOT_ERROR"
-			});
-		}
-	}
-};
-
-export const deleteTimeslot = (data) => {
-	return async (dispatch) => {
-		try {
-			await axios.delete("http://localhost:1337/database/timeslot", {data});
-			dispatch({
-				type: ADD_TIMESLOT,
-				payload: "SUCCESS"
-			});
-		} catch (error) {
-			dispatch({
-				type: ADD_TIMESLOT_ERROR,
-				payload: "DELETE_TIMESLOT_ERROR"
-			});
-		}
-	}
-}
-
 export const clickAddTimeslot = () => {
 	return (dispatch) => dispatch({ type: UI_CHANGE, payload: "CLICK_TIME_SLOT"});
-}
-
-export const clickEditTimeslot = () => {
-	return (dispatch) => dispatch({ type: UI_CHANGE, payload: "CLICK_TIME_SLOT_EDIT"});
-}
-
-export const clickDeleteTimeslot = () => {
-	return (dispatch) => dispatch({ type: UI_CHANGE, payload: "CLICK_TIME_SLOT_DELETE"});
 }
 
 export const cancelButton = () => {
