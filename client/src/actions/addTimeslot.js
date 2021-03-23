fd3b0ad92e7f4dcd207ab35bf612aaf4e96e0274
Sunny Pek <sunny1996@live.com.sg>
2021-03-23 10:21:43 +0800

    convert date object to ISOstring for database entry

--- client/src/actions/addTimeslot.js
+++ client/src/actions/addTimeslot.js
@@ -5,6 +5,14 @@ import { ADD_TIMESLOT_ERROR, ADD_TIMESLOT, UI_CHANGE } from "./types";
 export const addTimeslot = (data) => {
 	return async (dispatch) => {
 		try {
+			const startTime = data.Start_DateTime.toISOString().slice(0, 19).replace("T", " ");
+			const endTime = data.End_DateTime.toISOString().slice(0, 19).replace("T", " ");
+			const startHour = parseInt(startTime.slice(11,13)) + 8;
+			const endHour = parseInt(endTime.slice(11,13)) + 8;
+			const convertedStartTime = startTime.slice(0,11) + startHour.toString() + startTime.slice(13);
+			const convertedEndTime = endTime.slice(0,11) + endHour.toString() + endTime.slice(13);
+			data.Start_DateTime = convertedStartTime;
+			data.End_DateTime = convertedEndTime;
 			await axios.post("http://localhost:1337/database/timeslot", data);
 			dispatch({
 				type: ADD_TIMESLOT,
