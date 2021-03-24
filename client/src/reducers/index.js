0582652f2835355f3e940adb22f26c57e6c58e05
Sunny Pek <sunny1996@live.com.sg>
2021-03-24 16:30:43 +0800

    remove user reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,11 +2,9 @@ import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
 import authReducer from "./auth";
 import adminReducer from "./admin";
-import userReducer from "./user";
 
 export default combineReducers({
 	form: formReducer,
 	auth: authReducer,
-	admin: adminReducer,
-	user: userReducer
+	admin: adminReducer
 });
