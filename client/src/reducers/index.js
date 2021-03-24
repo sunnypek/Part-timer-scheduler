09c21604a0798c910b909e906d3eb7eed79e1a39
Sunny Pek <sunny1996@live.com.sg>
2021-03-24 13:05:18 +0800

    added user reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
 import authReducer from "./auth";
 import adminReducer from "./admin";
+import userReducer from "./user";
 
 export default combineReducers({
 	form: formReducer,
 	auth: authReducer,
-	admin: adminReducer
+	admin: adminReducer,
+	user: userReducer
 });
