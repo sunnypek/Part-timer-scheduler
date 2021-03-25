4a45c7653757d6c7607643fcc27ef511729e5bfd
Sunny Pek <sunny1996@live.com.sg>
2021-03-26 00:38:43 +0800

    added new booking controller

---
+++ server/controllers/booking.js
@@ -0,0 +1,24 @@
+const db = require("../database");
+
+module.exports = {
+    post: async (req, res, next) => {
+		try {
+			const result = await db.promise().query(
+				"INSERT INTO bookingdetail (Timeslot_ID, Employee_Name) VALUES (?, ?)",
+				[req.body.Timeslot_ID, req.body.Employee_Name]
+			);
+			res.status(200).json({ alreadybooked: false });	
+		} catch (error) {
+			console.error(error.sqlMessage);
+			res.status(200).json({ alreadybooked: true });
+		}
+	},
+
+	delete: async (req, res, next) => {
+		const result = await db.promise().query(
+			"DELETE FROM bookingdetail WHERE Timeslot_ID = ? AND Employee_Name = ?",
+			[req.query.timeslotID, req.query.employeeName]
+		);
+		res.status(200).json(result);
+	},
+}
