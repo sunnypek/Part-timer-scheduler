f81f9de32bb5e17521ee5d977390401276d01eae
Sunny Pek <sunny1996@live.com.sg>
2021-03-26 01:13:57 +0800

    fixed get bookings

--- server/controllers/booking.js
+++ server/controllers/booking.js
@@ -2,8 +2,16 @@ const db = require("../database");
 
 module.exports = {
     get: async (req, res, next) => {
-        
+        console.log(req.query);
+        await db.promise().query(
+            `SELECT * FROM bookingdetail WHERE Timeslot_ID = '${req.query.timeslotID}' AND Employee_Name != '${req.query.username}'`
+        ).then((result) => {
+            res.status(200).json(result[0]);
+        }).catch(() => {
+            res.status(500).json({ none: true });
+        });
     },
+
     post: async (req, res, next) => {
 		try {
 			const result = await db.promise().query(
