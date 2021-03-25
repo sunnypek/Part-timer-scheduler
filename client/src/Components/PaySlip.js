ae485372ddfb0f705acf11412b19a345e8a0ad04
KuraiSora16 <61778437+KuraiSora16@users.noreply.github.com>
2021-03-26 02:01:46 +0800

    Update PaySlip Dropdown Buttons

--- client/src/Components/PaySlip.js
+++ client/src/Components/PaySlip.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from "react"
-import DropdownButton from "react-bootstrap/DropdownButton"
+/* import DropdownButton from "react-bootstrap/DropdownButton"
 import Dropdown from "react-bootstrap/Dropdown"
+import { MonthPicker, YearPicker } from "react-dropdown-date"; */
 
 const column = {
     padding: "5px",
@@ -32,12 +33,12 @@ const divformat = {
 const fomatleft = {
     marginLeft: "auto"
 };
-const dropdownBtn = {
+/* const dropdownBtn = {
     backgroundColor: "#FFFFFF",
     padding: "28px",
     maxWidth: "10px",
     overflow: "hidden"
-};
+}; */
 
 function PaySlip(){
     const currentMonth = new Date().getMonth() + 1;
@@ -63,34 +64,44 @@ function PaySlip(){
             .then(data => setData(data));
     }, [year, month, username])
     
-    const yearArray = [];
+/*     const yearArray = [];
     const monthArray = [];
     const monthDropdownItems = [];
     const yearDropdownItems = [];
     const monthName = ["Month","January","February","March","April","May","June","July","August","September","October","November","December"];
-    for(var i = 0; i < 10; i++) {
+ */    /* for(var i = 0; i < 10; i++) {
         yearArray[i] = currentYear - i;
-    }
-    for(var j=1; j < 13; j++) {
+    } */
+    /* for(var j=1; j < 13; j++) {
         monthArray[j] = j ;
+    } */
+    const years = Array.from(new Array(22),(val, index) => currentYear - index);
+    const YrHandleChange = event => {
+        setYear(event.target.value);
     }
 
-    const YrHandleChange = event => {
-        setYear(event.target.text);
-    }
+    /* const YrLoad = event =>{
+        var ddlYears = document.getElementById("ddlYears");
+        for (var i = 1950; i <= currentYear; i++) {
+            var option = document.createElement("OPTION");
+            option = i;
+            option.value = i;
+            ddlYears.appendChild(option);
+        }
+    } */
 
     const MonHandleChange = event => {
-        var num = monthName.indexOf(event.target.text)
-        setMonth(num);
+        //var num = monthName.indexOf(event.target.text)
+        setMonth(event.target.value);
     }
 
-    for(const [index, value] of yearArray.entries()) {
+    /* for(const [index, value] of yearArray.entries()) {
         yearDropdownItems.push(<Dropdown.Item key={index} href="" onClick={YrHandleChange}>{value}</Dropdown.Item>);
     }
 
     for(const [index, value] of monthArray.entries()) {
         monthDropdownItems.push(<Dropdown.Item key={index} href="" onClick={MonHandleChange}>{monthName[value]}</Dropdown.Item>);
-    }
+    } */
 
     var hoursWorked = 0;
     var hoursOT = 0;
@@ -155,12 +166,48 @@ function PaySlip(){
             <h1>Welcome {username}</h1>
             <h2>My Payslips</h2>
             <div style={divformat}>
-                <DropdownButton id="dropdown-basic-button" title={monthName[month]} data-toggle="dropdown" bsPrefix={dropdownBtn}>
+                <select onClick={MonHandleChange}>
+                    <option defaultValue value='0'>Month</option>
+                    <option value='1'>January</option>
+                    <option value='2'>February</option>
+                    <option value='3'>March</option>
+                    <option value='4'>April</option>
+                    <option value='5'>May</option>
+                    <option value='6'>June</option>
+                    <option value='7'>July</option>
+                    <option value='8'>August</option>
+                    <option value='9'>September</option>
+                    <option value='10'>October</option>
+                    <option value='11'>November</option>
+                    <option value='12'>December</option>
+                </select>
+                {/* <YearPicker
+                    defaultValue={'Year'}
+                    start={2000}
+                    reverse                     // default is ASCENDING
+                    required={true}             // default is false
+                    disabled={false}             // default is false
+                    value={year}     // mandatory
+                    onChange={YrHandleChange}
+                    id={'year'}
+                    name={'year'}
+                    classes={'classes'}
+                    optionClasses={'option classes'}
+                /> */}
+                <select id='date-dropdown' onChange={YrHandleChange}>
+                    <option defaultValue value='0'>Year</option>
+                {
+                    years.map((year, index) => {
+                        return <option key={`year${index}`} value={year}>{year}</option>
+                    })
+                }
+                </select>
+                {/* <DropdownButton id="dropdown-basic-button" title={monthName[month]} data-toggle="dropdown" bsPrefix={dropdownBtn}>
                     {monthDropdownItems}
                 </DropdownButton>
                 <DropdownButton id="dropdown-basic-button" title={year} data-toggle="dropdown" bsPrefix={dropdownBtn}>
                     {yearDropdownItems}
-                </DropdownButton>
+                </DropdownButton> */}
             </div>
 
             <div style={divformat}>
